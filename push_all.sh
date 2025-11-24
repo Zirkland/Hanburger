8c74178f5b7318fc5a6790987ae076e54dd69cde
HarveyBlocks <harvey.blocks@outlook.com>
2025-11-24 21:52:32 +0800

    fix: fix shell script

--- push_all.sh
+++ push_all.sh
@@ -1,13 +1,8 @@
 ﻿# 从上游仓库获取最新更改
-git fetch upstream
-
-# 切换到你的主分支（通常是 main 或 master）
-git checkout main
-
 # 合并上游仓库的更改
+git fetch upstream
+git checkout main
 git merge upstream/main
 
-# 将合并后的更改推送到你的 GitHub fork
-git push origin main
-
+# GitHub fork
 git push origin main
