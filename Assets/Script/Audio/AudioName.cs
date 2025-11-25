ba7022ffa9a10b3485c11c7e6154357927aafa97
HarveyBlocks <harvey.blocks@outlook.com>
2025-11-25 15:12:48 +0800

    fix: Mobile ingredients drag and drop

---
+++ Assets/Script/Audio/AudioName.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioName
+{
+    public static string Start { get => "Start"; }
+    public static string Boss1 { get => "Boss1"; }
+    public static string Boss2 { get => "Boss2"; }
+    public static string Boss3 { get => "Boss3"; }
+    public static string Success { get => "Success"; }
+    public static string Failure { get => "Failure"; }
+    public static string ESC { get => "ESC"; }
+    public static string Button { get => "Button"; }
+    public static string Switch { get => "Switch"; }
+    public static string Publish { get => "Publish"; }
+    public static string Abandon { get => "Abandon"; }
+    public static string CookCard { get => "CookCard"; }
+}
